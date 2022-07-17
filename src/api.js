faddd97076693da38067d9018395f5baca520342
Asher-jay Arenz <asher-jayarenz@Asher-jays-MacBook-Air.local>
2022-07-17 14:52:34 +0900

    React Weather Application

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ee9204604msh44ca564cde4f576p1e34f9jsn30b6f2cebadb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "518bb20f095fcc086a6a7b8142a2a1fa";
